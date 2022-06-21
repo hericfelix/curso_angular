fa300dba5b2d25f862778fb45b90a4276c9447ec
Heric Felix <hfelixmartins@gmail.com>
2022-06-20 23:18:03 -0300

    feat: exercicio1 angular completed

--- semana2/exercicios_angular/exercicio1/src/app/app.module.ts
+++ semana2/exercicios_angular/exercicio1/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentComponent } from './home/components/content/content.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MainComponent } from './home/pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
